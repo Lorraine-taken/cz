5f06ec33ad0c9e149b71f18b683d005492fb3f41
Lorraine-taken <lc5713@126.com>
2017-09-15 16:41:07 +0800

    modify

---
+++ js/markers2.js
@@ -0,0 +1,46 @@
+var provinces = [{
+	"name": "三里屯",
+	"center": "116.455294,39.937492",
+	"count": 25500,
+	"subDistricts": [{
+		"name": "三里屯太古里北区",
+		"center": "116.454507,39.938515",
+		"count": 100,
+		"link": "tansuo_detail2.html"
+	}, {
+		"name": "see party summer",
+		"center": "116.454229,39.936446",
+		"count": 150,
+		"link": "tansuo_detail2.html"
+	}, {
+		"name": "Apple三里屯",
+		"center": "116.454352,39.935043",
+		"count": 300,
+		"link": "tansuo_detail2.html"
+	}, {
+		"name": "渡金湖",
+		"center": "116.453794,39.938062",
+		"count": 250,
+		"link": "tansuo_detail2.html"
+	}, {
+		"name": "三里屯SOHO",
+		"center": "116.453649,39.932176",
+		"count": 150,
+		"link": "tansuo_detail2.html"
+	}, {
+		"name": "盈科大厦",
+		"center": "116.458789,39.932625",
+		"count": 350,
+		"link": "tansuo_detail2.html"
+	}, {
+		"name": "北京三里屯CHAO",
+		"center": "116.451536,39.932749",
+		"count": 150,
+		"link": "tansuo_detail2.html"
+	}, {
+		"name": "美嘉欢乐影城",
+		"center": "116.454454,39.934509",
+		"count": 100,
+		"link": "tansuo_detail2.html"
+	}]
+}];
--- js/videoEvent.js
+++ js/videoEvent.js
@@ -1,7 +1,7 @@
 $(function(){
-	$('.vbox').on('click', function(){
-		var url = $(this).attr('jump_data');
-		window.location.href=url;
-	})
+	// $('.vbox').on('click', function(){
+	// 	var url = $(this).attr('jump_data');
+	// 	window.location.href=url;
+	// })
 	
 })
