4a89f5bb60b8035287676f7b4ad55f7434e857e7
Lorraine-taken <lc5713@126.com>
2017-09-20 13:06:10 +0800

    modify

--- js/style.js
+++ js/style.js
@@ -1,3 +1,16 @@
-$('.clickbg').click(function(){
-	alert(1);
+$(function(){
+	$('.operate .thumbs-up').click(function(){
+	  if($(this).children().hasClass('thub')){
+	    $(this).children().removeClass('thub');
+	  }else{
+	    $(this).children().addClass('thub');
+	  }              
+	})
+	$('.commbox .dz').click(function(){
+	  if($(this).hasClass('thub')){
+	    $(this).removeClass('thub');
+	  }else{
+	    $(this).addClass('thub');
+	  }              
+	})
 })
